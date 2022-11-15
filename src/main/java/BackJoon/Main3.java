a7c46b1574c46a4def97a949b2c97b59dd29d507
tmddk9445 <tmddk9445@naver.com>
2022-11-15 12:38:53 +0900

    BackJoon 파트1 입출력과 사칙연산

---
+++ src/main/java/BackJoon/Main3.java
@@ -0,0 +1,31 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int e = 0;
+        int f = 0;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+        d = scanner.nextInt();
+        e = scanner.nextInt();
+        f = scanner.nextInt();
+
+        System.out.println(a - 1);
+        System.out.println(b - 1);
+        System.out.println(c - 2);
+        System.out.println(d - 2);
+        System.out.println(e - 2);
+        System.out.println(f - 8);
+    }
+}
---
+++ src/main/java/BackJoon/Main4.java
@@ -0,0 +1,19 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
