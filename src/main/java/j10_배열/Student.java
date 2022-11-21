40db1e24dd7710cbdbac10a42cef8ebb2f83a468
tmddk9445 <tmddk9445@naver.com>
2022-11-21 12:50:15 +0900

    2022-11-21 월 j10_배열

---
+++ src/main/java/j10_배열/Student.java
@@ -0,0 +1,28 @@
+package j10_배열;
+
+public class Student {
+
+    private String name;
+    private int year;
+
+    public Student(String name, int year) { // Alt + insert
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
