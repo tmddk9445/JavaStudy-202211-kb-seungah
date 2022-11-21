e91ab687701d896808fa0ca5ef7da24f8a07b8d9
tmddk9445 <tmddk9445@naver.com>
2022-11-21 12:49:44 +0900

    2022-11-21 월 j10_배열
    과제 풀이

--- src/main/java/j10_배열/Array4.java
+++ src/main/java/j10_배열/Array4.java
@@ -2,22 +2,99 @@ package j10_배열;
 
 public class Array4 {
 
-    public static int cmpMin(int[] array1, int[] array2) {
-        int min = 0;
+    public static int[] add(int[] array, int value) { // add배열 - 새로운 배열을 생성
+        int[] resultArray = new int[array.length + 1]; // 기존의 배열과 동일하게 옮긴 뒤 마지막 인덱스에 마지막배열의 크기가 들어간다.
+        for (int i = 0; i < array.length; i++) {
 
+            resultArray[i] = array[i];
+        }
+        resultArray[array.length] = value;
+        return resultArray;
+    }
+
+    public static int[] getDuplicateArray(int[] array1, int[] array2) { // 중복된 Array를 만드는 식
+        int[] resultArray = new int[0]; // 배열의 공간이 비어있다.
+
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    resultArray = add(resultArray, array1[i]);
+                }
+            }
+        }
+        return resultArray;
+    }
+
+    public static int[] getNonDuplicateArray(int[] array1, int[] array2) {
+
+        int[] resultArray = new int[0];
+
+        for (int i = 0; i < array1.length; i++) {
+            int duplicateCountInArray1 = 0;
+
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    duplicateCountInArray1++;
+                }
+            }
+            if(duplicateCountInArray1 != 0) {
+                continue;
+            }
+            resultArray = add(resultArray, array1[i]);
+        }
+
+        for (int i = 0; i < array1.length; i++) {
+            int duplicateCountInArray2 = 0;
+
+            for (int j = 0; j < array2.length; j++) {
+                if (array2[i] == array1[j]) {
+                    duplicateCountInArray2++;
+                }
+            }
+            if(duplicateCountInArray2 != 0) {
+                continue;
+            }
+            resultArray = add(resultArray, array2[i]);
+        }
+
+        return resultArray;
+    }
+
+    public static int getMinNumberInArray(int[] array) {
+        int min = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
         return min;
     }
 
-    public static int notCmpMin(int[] array1, int[] array2) {
-        int max = 0;
+    public static int getMaxNumberInArray(int[] array) {
+        int max = array[0];
 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
         return max;
     }
 
     public static void main(String[] args) {
 
-        int[] nums1 = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] nums2 = new int[] {6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int[] nums1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] nums2 = new int[]{6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+        int[] duplicateArray = getDuplicateArray(nums1, nums2);
+        int[] nonDuplicateArray = getNonDuplicateArray(nums1, nums2);
+
+        int minNumberInDuplicateArray = getMinNumberInArray(duplicateArray);
+        int maxNumberInNonDuplicateArray = getMaxNumberInArray(nonDuplicateArray);
+
+        System.out.println("중복된 값 중 최소값 : " + minNumberInDuplicateArray);
+        System.out.println("중복되지 않은 값 중 최대값 : " + maxNumberInNonDuplicateArray);
 
         /*
         1. 두 배열에서 중복된 값 중 최소값
--- src/main/java/j10_배열/Array5.java
+++ src/main/java/j10_배열/Array5.java
@@ -1,17 +1,27 @@
 package j10_배열;
 
 public class Array5 {
+
+    public static int getMinNumbner(int nums1, int nums2) {
+        return nums1 < nums2 ? nums1 : nums2;
+    }
+
     public static void main(String[] args) {
-        int[] num1 = new int[]{1, 8, 9, 16, 17};
-        int[] num2 = new int[]{2, 7, 10, 15, 18};
-        int[] num3 = new int[]{3, 6, 11, 14, 19};
-        int[] num4 = new int[]{4, 5, 12, 13, 20};
+        int[] nums1 = new int[]{1, 8, 9, 16, 17};
+        int[] nums2 = new int[]{2, 7, 10, 15, 18};
+        int[] nums3 = new int[]{3, 6, 11, 14, 19};
+        int[] nums4 = new int[]{4, 5, 12, 13, 20};
 
         int[] resultArray = new int[5];
 
         /*
-        배열들 중 동일 인덱스에 위cl한 값들 중 최소값들만 뽑아서 새로운 배열을 만든다.
+        배열들 중 동일 인덱스에 위치한 값들 중 최소값들만 뽑아서 새로운 배열을 만든다.
          */
+
+        for(int i = 0; i < nums1.length; i++){
+            resultArray[i] = getMinNumbner(getMinNumbner(nums1[i], nums2[i]), getMinNumbner(nums3[i], nums4[i]));
+            System.out.println(resultArray[i]);
+        }
 
         int a = 10;
         int b = 5;
